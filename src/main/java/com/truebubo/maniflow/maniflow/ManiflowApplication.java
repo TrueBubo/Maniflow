fcef632bd22418b12af9093b16a1f223addbe4c2
Filip Bubák <bubak.filip@pm.me>
2025-05-03 22:38:43 +0200

    Remove mongodb init logs

--- src/main/java/com/truebubo/maniflow/maniflow/ManiflowApplication.java
+++ src/main/java/com/truebubo/maniflow/maniflow/ManiflowApplication.java
@@ -2,14 +2,19 @@ package com.truebubo.maniflow.maniflow;
 
 import com.truebubo.maniflow.UIMode.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
 
-import java.util.function.Consumer;
 
 import static java.util.Arrays.stream;
 
 @SpringBootApplication
 public class ManiflowApplication {
     public static void main(String[] args) {
+        // Remove initialization mongo logs
+        ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger("org.mongodb.driver").setLevel(Level.ERROR);
+
         UIMode mode = stream(args).anyMatch(arg -> arg.equals("-g") || arg.equals("--gui"))
             ? new GUI()
             : new CLI();
